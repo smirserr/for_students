4228516d073c63b3b9b18d14183fa56e1b967590
smirserr <smirserr@gmail.com>
2020-06-18 18:22:21 +0300

    update 916.cpp

--- acmp.ru/916.cpp
+++ acmp.ru/916.cpp
@@ -1,26 +1,28 @@
-#include <iostream> 
+#include <iostream>
 #include <algorithm>
-using namespace std;    
- 
+using namespace std;
+
+
 int main()
 {
-    int n, k;
-    cin >> n >> k;
-    int a[n];
-    for(int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    sort(a, a+n);
- 
-    long long int sum = 0;
-    for(int i = 0; i < n; i++)
-    {
-        int t = (n-i) / k;
-        if ((n-i) % k != 0) t++;
-        sum += a[i]*t; 
-    }
-    cout << sum << endl;
- 
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for(int i = 0; i < n; i++) cin >> a[i];
+	sort(a, a+n);
+	long long int sum = 0;
+
+	int p = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if (i % k == 0) p ++;
+		sum += a[n-1-i] * p;
+	}
+
+	cout << sum << endl;
+	
+
+	
+
     return 0;
 }
