804ecfc26c119cb6319bc5579f321268d64c9ff5
smirserr <smirserr@gmail.com>
2021-01-19 19:31:05 +0300

    add 672.cpp

---
+++ acmp.ru/672.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == 1)
+        cout << "10 0" << endl;
+    if (n == 2)
+        cout << "1 22" << endl;
+    if (n == 3)
+        cout << "6 123" << endl;
+    if (n == 4)
+        cout << "12 1124" << endl;
+    if (n == 5)
+        cout << "40 11125" << endl;
+    if (n == 6)
+        cout << "30 111126" << endl;
+    if (n == 7)
+        cout << "84 1111127" << endl;
+    if (n == 8)
+        cout << "224 11111128" << endl;
+    if (n == 9)
+        cout << "144 111111129" << endl;
+    if (n == 10)
+        cout << "45 1111111144" << endl;
+
+
+    return 0;
+}
